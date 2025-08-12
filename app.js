8e54c359acdc8f35b4a836b0c7761e4c9c990033
rana-ashutosh <ashutoshr033@gmail.com>
2025-08-12 09:52:24 +0530

    Today Learn how to update the current user details and also completed CRUD operation in Backend

--- app.js
+++ app.js
@@ -7,61 +7,42 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// const useModel = require('./userModel');
 const userModel = require('./models/user')
 
 app.get('/', (req, res) => {
      res.render('index');
    });
     
-// Create, Read, Update, Delete
-// app.get('/create', async (req, res) => {
-//   let user = await useModel.create({
-//     name:"ashu",
-//     username: 'ashu@123',
-//     email: 'ashu123@gmail.com'
-//    });
-
-//    res.send(user);
-
-// });
-
+app.post('/create', async (req, res) => {
+   let {name, email, image} = req.body;
+   
+   let createdUser = await userModel.create({
+      name, email, image
+   })
+   
+   res.redirect('/read');
+});
+   
 app.get('/read', async(req, res) => {
    let allusers = await userModel.find();
 
    res.render('read', {allusers});
+});
+   
+app.get('/edit/:userid', async (req, res) => {
+   let user = await userModel.findOne({_id: req.params.userid});
+   res.render("edit", {user});
+});
+
+app.post('/update/:userid', async (req, res) => {
+   let {image, name, email} = req.body;
+   let user = await userModel.findOneAndUpdate({_id: req.params.userid}, {image, name, email}, {new:true});
+   res.redirect("/read");
 });
 
 app.get('/delete/:id', async(req, res) => {
    let deleteUser = await userModel.findOneAndDelete({_id: req.params.id});
    res.redirect('/read');
 });
-
-app.post('/create', async (req, res) => {
-    let {name, email, image} = req.body;
-
-    let createdUser = await userModel.create({
-        name,
-        email,
-        image
-    })
-
-    res.redirect('/read');
-})
-
-// app.get('/update', async (req, res) => {
-//     let updatedUser = await useModel.findOneAndUpdate({username: 'ashu@123'}, {name: 'Ashutosh Rana'}, {new: true});
-    
-//     res.send(updatedUser);
-   
-// });
-
-// app.get('/delete', async (req, res) => {
-//     let deleteUser = await userModel.findOneAndDelete({username: 'ashu@123'});
-
-//     res.send(deleteUser);
-// });
-
-
 
 app.listen(3000);
