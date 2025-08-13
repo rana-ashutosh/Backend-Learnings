4a7dca19abcbb943aa3e049b58c7afc869387a6e
rana-ashutosh <ashutoshr033@gmail.com>
2025-08-13 10:18:35 +0530

    Today learn Authentication, Authorization and how to set cookies

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser())
 
-const userModel = require('./models/user')
+const userModel = require('./models/user');
 
 app.get('/', (req, res) => {
      res.render('index');
@@ -44,5 +46,15 @@ app.get('/delete/:id', async(req, res) => {
    let deleteUser = await userModel.findOneAndDelete({_id: req.params.id});
    res.redirect('/read');
 });
+
+app.get('/cookies', function (req, res) {
+   res.cookie('name', 'ashutosh');
+   res.send('done')
+});
+
+app.get('/cookieRead', function (req, res) {
+   console.log(req.cookies);
+   res.send('read page')
+})
 
 app.listen(3000);
